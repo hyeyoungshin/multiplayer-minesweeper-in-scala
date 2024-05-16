2b13071a57b1d966c78a18f4f680efaa879fce07
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-05-16 17:20:14 -0600

    apply changes in tests

--- scala-3-multi-player-mindsweeper/src/test/scala/minesweeper.scala
+++ scala-3-multi-player-mindsweeper/src/test/scala/minesweeper.scala
@@ -2,55 +2,64 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class Minesweeper extends AnyFunSuite {
   val mine_locations = Array(Array(0, 0, 1), Array(0, 0, 0), Array(0, 0, 1))
-
+  
   val filename = "src/test/board_tests/1-in.json"
-  val game_input = parse_game(filename)
+  val game_input = parse_game_input(filename)
+  val mineboard = create_mineboard(game_input.board)  
+  val click = Coordinate(game_input.reveal.head.row - 1, game_input.reveal.head.column - 1)
 
-  test("count_mine") {
-    val res = count_mine(mine_locations, 1, 2)
+  val playerboard = Board(
+    xsize = 3,
+    ysize = 3,
+    tile_map = Map(Coordinate(0, 0) -> PlayerTile.Hidden, Coordinate(0, 1) -> PlayerTile.Hidden, Coordinate(0, 2) -> PlayerTile.Hidden,
+                   Coordinate(1, 0) -> PlayerTile.Hidden, Coordinate(1, 1) -> PlayerTile.Hidden, Coordinate(1, 2) -> PlayerTile.Hidden,
+                   Coordinate(2, 0) -> PlayerTile.Hidden, Coordinate(2, 1) -> PlayerTile.Hidden, Coordinate(2, 2) -> PlayerTile.Hidden
+    )
+  )
+
+  
+
+  test("count_neighboring_mines") {
+    val res = count_neighboring_mines(mineboard, click)
     assert(res == 2)
   }
 
-  test("parse_game") {
+  test("parse_game_input") {
     val filename = "src/test/board_tests/1-in.json"
-    val game_input = parse_game(filename)
+    val game_input = parse_game_input(filename)
 
     assert(game_input.board.length == 3)
     assert(game_input.reveal.head.row == 2)
   }
 
-  test("create_tile-hint") {
-    val loc = (1, 2)
-    val test_tile = create_tile(mine_locations, loc._1, loc._2)
+  test("get_solutiontile_at-hint") {
+    val test_tile = get_solutiontile_at(mineboard, click)
     
     assert(test_tile == SolutionTile.Hint(2))
   }
 
-  test("create_tile-empty") {
-    val loc = (0, 0)
-    val test_tile = create_tile(mine_locations, loc._1, loc._2)
+  test("get_solutiontile_at-empty") {
+    val test_tile = get_solutiontile_at(mineboard, Coordinate(0, 0))
     
     assert(test_tile == SolutionTile.Empty)
   }
 
-  test("create_tile-mine") {
-    val loc = (0, 2)
-    val test_tile = create_tile(mine_locations, loc._1, loc._2)
+  test("get_solutiontile_at-mine") {
+    val test_tile = get_solutiontile_at(mineboard, Coordinate(0, 2))
     
     assert(test_tile == SolutionTile.Mine)
   }
 
-  test("create_board") {
+  test("create_solutiboard") {
+    val solutionboard = create_solutionboard(mineboard)
     
-    assert(game_input.board.length == 3)
-    assert(game_input.reveal.head.row == 2)
+    assert(solutionboard.tile_map(Coordinate(1, 2)) == SolutionTile.Hint(2))
   }
 
   test("reveal") {
-    val filename = "src/test/board_tests/1-in.json"
-    val game_input = parse_game(filename)
+    val solutionboard = create_solutionboard(mineboard)
+    val next_playerboard = reveal(solutionboard, playerboard, click)
 
-    assert(game_input.board.length == 3)
-    assert(game_input.reveal.head.row == 2)
+    assert(next_playerboard.tile_map(click) == PlayerTile.Revealed(SolutionTile.Hint(2)))
   }
 }
