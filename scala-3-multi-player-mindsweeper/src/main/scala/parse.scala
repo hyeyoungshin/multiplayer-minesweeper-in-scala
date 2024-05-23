e34147b8617d4270889d69d70dcfff16d5a48898
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-05-23 13:41:35 -0600

    remove create_mineboard; it's in board.scala

--- scala-3-multi-player-mindsweeper/src/main/scala/parse.scala
+++ scala-3-multi-player-mindsweeper/src/main/scala/parse.scala
@@ -6,8 +6,6 @@ case class GameInput (board: Array[Array[Int]], reveal: List[InputCoordinate]) d
 // 1-based indexing
 case class InputCoordinate(row: Int, column: Int) derives ReadWriter
 
-def create_mineboard(mine_locations: Array[Array[Int]]): MineBoard = ???
-
 // Takes a json file and returns GameInput which contains
 // 1. (mine)board : location of mines in a matrix represented by Array of Arrays
 // 2. reveal : location of a tile in the matrix
