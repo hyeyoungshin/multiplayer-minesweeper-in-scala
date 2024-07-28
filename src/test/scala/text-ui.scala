be9517edc0d00d096f690cbf5bdb3a55603af2d2
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-07-28 14:34:27 -0600

    same changes to text-ui test

--- src/test/scala/text-ui.scala
+++ src/test/scala/text-ui.scala
@@ -6,7 +6,7 @@ class TextUI extends AnyFunSuite {
   test("player action") {
     val new_state = play(state, PlayerAction(Action.Reveal, Coordinate(1, 1)))
     val current_player = new_state.player_pool.current()
-    assert(current_player.board.tile_map(Coordinate(1, 1)) != PlayerTile.Hidden)
+    assert(current_player.board.tile_map(Coordinate(1, 1)) != PlayerTile(None, None))
   }
 
 }
