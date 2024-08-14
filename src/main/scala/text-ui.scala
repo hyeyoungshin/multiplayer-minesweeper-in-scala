8a38c563eef944a57e82de10356b404a4edf5986
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-08-14 10:45:05 -0600

    fixed a bug in the main loop and else

--- src/main/scala/text-ui.scala
+++ src/main/scala/text-ui.scala
@@ -12,16 +12,15 @@ import upickle.implicits.Readers
   print_difficulty(difficulty)
 
   var state = new_game(difficulty)
-  print_state(state)
+  // print_state(state)
 
-  while !game_over(state) do 
-    print_state(state)
+  while !game_over(state.status) do 
+    print_board(state.playerpool.current(), state.solution.num_mines)
     val player_action = get_valid_inputs(state)
-    // val coordinate = get_valid_coordinate(state)
-    // val player_action = get_valid_action(state)(coordinate)
-    state = play(state, player_action)
-    print_state(state)
-    state = next_player(state)
+    val new_state = play(state, player_action)
+    print_board(new_state.playerpool.current(), state.solution.num_mines)
+    print_status(new_state.status)
+    state = next_player(new_state)
     
 
 
@@ -91,9 +90,9 @@ def parse_difficulty(user_input: String): Either[String, GameDifficulty] = {
 // Coordinate //
 ////////////////
 def get_valid_coordinate(state: GameState): Coordinate = {
-  val current_player = state.player_pool.current()
+  val current_player = state.playerpool.current()
 
-  get_valid_input(message = s"Player ${current_player.id.n}, enter a tile position:",
+  get_valid_input(message = s"Player ${current_player.id}, enter a tile position:",
                   parse_and_validate = input => parse_coordinate(input).flatMap(x => valid_coordinate(state, x)))
 }
     
@@ -105,7 +104,7 @@ int array of length other than 2 is considered invalid
 def parse_coordinate(user_input: String): Either[String, Coordinate] =
   val option_parsed = parse_coordinate_helper(user_input)
   option_parsed.filter(arr => arr.length == 2) match {
-    case Some(arr) => Right(convert_input_coordinate(InputCoordinate(arr(0), arr(1))))
+    case Some(arr) => Right(convert_input_coordinate_to_coordinate(InputCoordinate(arr(0), arr(1))))
     case None => Left("The tile pos is in wrong format.")
   }
 
@@ -124,11 +123,11 @@ user input coordinate is valid when it is within board boundary AND
 the tile at input coordinate is not revealed
  */
 def valid_coordinate(state: GameState, tile_pos: Coordinate): Either[String, Coordinate] = 
-  val current_player = state.player_pool.current()
+  val current_player = state.playerpool.current()
   if current_player.board.within_boundary(tile_pos) then
     current_player.board.tile_map(tile_pos) match {
       case PlayerTile.Revealed(_) => Left("The tile is alreay revealed.")
-      case PlayerTile.RNF(_, _) => Left("The tile is alreay revealed.")
+      case PlayerTile.RevealedNFlagged(_, _) => Left("The tile is alreay revealed.")
       case _ => Right(tile_pos)
     }
   else 
@@ -154,16 +153,16 @@ def parse_action(input: String): Either[String, Action] =
 
 
 def valid_player_action(state: GameState, player_action: PlayerAction): Either[String, PlayerAction] = {
-  val current_player = state.player_pool.current()
-  val p_tile = current_player.board.tile_map(player_action.pos)
+  val current_player = state.playerpool.current()
+  val p_tile = current_player.board.tile_map(player_action.tile_pos)
   
 
   player_action.action match {
     case Action.Flag => p_tile match {
       case PlayerTile.Hidden => Right(player_action) 
-      case PlayerTile.Flagged(by) => Left(s"The tile is already flagged by Player ${by.id.n}.")
+      case PlayerTile.Flagged(flagger) => Left(s"The tile is already flagged by Player ${flagger.id}.")
       case PlayerTile.Revealed(_) => Left(s"The tile is already revealed.")
-      case PlayerTile.RNF(_, _) => Left(s"The tile is already revealed.")
+      case PlayerTile.RevealedNFlagged(_, _) => Left(s"The tile is already revealed.")
 
       
       // case PlayerTile.Hidden => Right(player_action) 
@@ -179,16 +178,16 @@ def valid_player_action(state: GameState, player_action: PlayerAction): Either[S
         else
           Left(s"The tile is already flagged.")
       case PlayerTile.Revealed(_) => Left(s"The tile is already revealed.")
-      case PlayerTile.RNF(_, _) => Left(s"The tile is already revealed.")
+      case PlayerTile.RevealedNFlagged(_, _) => Left(s"The tile is already revealed.")
     }
     
     case Action.Unflag => p_tile match {
-      case PlayerTile.Flagged(by) => 
-        if by.id == current_player.id then
+      case PlayerTile.Flagged(flagger) => 
+        if flagger.id == current_player.id then
           Right(player_action)
         else
-          Left(s"The tile is already flagged by Player ${by.id.n}.") 
-      case PlayerTile.RNF(_, _) => Left("The tile is already revealed.")
+          Left(s"The tile is already flagged by Player ${flagger.id}.") 
+      case PlayerTile.RevealedNFlagged(_, _) => Left("The tile is already revealed.")
       case _ => Left("You cannot unflag a tile that's not flagged.") 
       }
     }
@@ -220,7 +219,7 @@ def print_inplace(): Unit =
 
 def print_difficulty(difficulty: GameDifficulty): Unit = {
   println("")
-  print_with_effect(s"Starting a game with board size: (${difficulty.size._1} x ${difficulty.size._2}) " +
+  print_with_effect(s"Starting a game with board size: (${difficulty.board_size._1} x ${difficulty.board_size._2}) " +
     s"and Number of mines: ${difficulty.num_mines}", PrinterEffects.Bold)
   println("")
  
@@ -228,41 +227,41 @@ def print_difficulty(difficulty: GameDifficulty): Unit = {
 }
 
 
-def print_state(state: GameState): Unit = 
-  print_inplace()
-  val current_player = state.player_pool.current()
-  print_with_effect(s"Board: Player ${current_player.id.n}", PrinterEffects.Bold)
-  print_with_effect(s"Number of Mines: ${state.solution.num_mines}", PrinterEffects.Bold)
-
-  print_board(state.player_pool.current().board)
-  Thread.sleep(500)
-
-  state.status match {
-    case GameStatus.Win(player) => print_with_effect(s"Player ${player.id.n} win!", PrinterEffects.Bold)
-    case GameStatus.Lose => print_with_effect(s"Player ${current_player.id.n} lost...", PrinterEffects.Bold)
+def print_status(status: GameStatus): Unit = 
+  status match {
+    case GameStatus.Win(winners) => winners.foreach(winner => print_with_effect(s"Player ${winner.id} win!", PrinterEffects.Bold))
     case GameStatus.Continue => ()
   }
 
 
-def print_board(player_board: PlayerBoard): Unit = 
-  val str_board = Array.fill(player_board.xsize)(Array.fill(player_board.ysize)(""))
-  player_board.tile_map.map((tile_pos, tile) => str_board(tile_pos._1)(tile_pos._2) = player_tile_to_string(tile))
-  print_helper[String](str_board)
+def print_board(player: Player, num_mines: Int): Unit = {
+  print_inplace()
 
+  print_with_effect(s"Board: Player ${player.id}", PrinterEffects.Bold)
+  print_with_effect(s"Number of Mines: ${num_mines}", PrinterEffects.Bold)
 
-def solution_tile_to_string(s_tile: SolutionTile): String = s_tile match {
-  case SolutionTile.Empty => "E"
-  case SolutionTile.Mine => "x"
-  case SolutionTile.Hint(n) => s"$n"
+  player.board.print_board_for_test()
+
+  Thread.sleep(500)
 }
 
 
-def player_tile_to_string(p_tile: PlayerTile): String = p_tile match {
-  case PlayerTile.Hidden => "[   ]"
-  case PlayerTile.Flagged(by) => s"[ ${by.color.code}F${PrinterEffects.Reset} ]"
-  case PlayerTile.Revealed(s_tile) => s"[ ${solution_tile_to_string(s_tile)} ]"
-  case PlayerTile.RNF(s_tile, by) => 
-    s"[${solution_tile_to_string(s_tile)}|${by.color.code}F${PrinterEffects.Reset}]"
+def tile_to_string[T](tile: T): String = tile match {
+  case t: SolutionTile => t match {
+    case SolutionTile.Empty => "0"
+    case SolutionTile.Mine => "x"
+    case SolutionTile.Hint(n) => s"$n"
+  }
+  case t: PlayerTile => t  match {
+    case PlayerTile.Hidden => "[   ]"
+    case PlayerTile.Flagged(flagger) => s"[ ${flagger.color.code}F${PrinterEffects.Reset} ]"
+    case PlayerTile.Revealed(s_tile) => s"[ ${tile_to_string(s_tile)} ]"
+    case PlayerTile.RevealedNFlagged(s_tile, flagger) => s"[ ${tile_to_string(s_tile)}|${flagger.color.code}F${PrinterEffects.Reset} ]"
+  }
+  case t: Boolean => t match {
+    case true => "[*]" // mine
+    case false => "[ ]"
+  }
 }
 
 
@@ -270,8 +269,8 @@ def player_tile_to_string(p_tile: PlayerTile): String = p_tile match {
 // ** You give
 // board : Array of Arrays of type T, where T can be Int for input board or String for Tile
 // ** You get
-// print out of the board  
-def print_helper[T](board: Array[Array[T]]): Unit = 
+// printed board in matrix format 
+def print_as_matrix[T](board: Array[Array[T]]): Unit = 
   println(board.map(_.mkString("")).mkString("\n"))
   println("")
 
--- src/test/scala/text-ui.scala
+++ src/test/scala/text-ui.scala
@@ -1,17 +1,201 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class TextUI extends AnyFunSuite {
-  var state = new_game(Easy)
+  var new_state = GameState(Solution(solution_board, 2),
+                            PlayerPool(List(player_0, player_1), 0),
+                            GameStatus.Continue)
 
+  val test_pos = Coordinate(3, 3)
+
+  val player_0 = Player(0, new_player_board, PlayerColor(PrinterEffects.Red))
+  val player_1 = Player(1, new_player_board, PlayerColor(PrinterEffects.Blue))
+  
+  // [E][2][x]
+  // [E][2][x]
+  // [E][1][1]
+  val solution_board = Board[SolutionTile](3, 3, Map(
+      (Coordinate(1,1) -> SolutionTile.Empty), (Coordinate(1,2) -> SolutionTile.Hint(2)), (Coordinate(1,3) -> SolutionTile.Mine), 
+      (Coordinate(2,1) -> SolutionTile.Empty), (Coordinate(2,2) -> SolutionTile.Hint(2)), (Coordinate(2,3) -> SolutionTile.Mine), 
+      (Coordinate(3,1) -> SolutionTile.Empty), (Coordinate(3,2) -> SolutionTile.Hint(1)), (Coordinate(3,3) -> SolutionTile.Hint(1))
+    ))
+
+  val new_player_board = Board[PlayerTile](3, 3, Map(
+    (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Hidden), 
+    (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Hidden), 
+    (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+  ))
+
+
+  /*
   test("player action") {
-    val new_state = play(state, PlayerAction(Action.Reveal, Coordinate(1, 1)))
-    val current_player = new_state.player_pool.current()
-    assert(current_player.board.tile_map(Coordinate(1, 1)) != PlayerTile(None, None))
+    println(s"new_state board: ${new_state.player_pool.current().board}")
+
+    val next_state = play(new_state, PlayerAction(Action.Reveal, test_pos))
+    val current_player = next_state.player_pool.current()
+    
+    assert(current_player.board.tile_map(test_pos) != PlayerTile.Hidden)
+    assert(current_player.board.tile_map(test_pos) == PlayerTile.Revealed(SolutionTile.Hint(1)))
   }
 
-}
+  test("play-reveal-test_pos") {
+    val next_game_state = play(new_state, PlayerAction(Action.Reveal, test_pos))
+    println(s"current_player is: ${next_game_state.player_pool.current().id.n}")
+    println(s"after play: ${next_game_state.player_pool.current().board.tile_map(test_pos)}")
+    
+    assert(next_game_state.player_pool.current().board.tile_map(test_pos) == PlayerTile.Revealed(SolutionTile.Hint(1)))
+  }
 
-// should not win
-// [E][1][ ]
-// [E][2][ ] 
-// [E][2][F]
+  test("all_mines_flagged-true") {
+    // [ ][ ][F]
+    // [ ][ ][F]
+    // [ ][ ][ ]
+    val test_board = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(Player(PlayerID(0), new_player_board, PlayerColor(PrinterEffects.Red)))), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(Player(PlayerID(1), new_player_board, PlayerColor(PrinterEffects.Blue)))), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    assert(all_mines_flagged(test_board, solution_board) == true)
+  }
+
+  test("all_mines_flagged-false") {
+    // [][][ ]
+    // [][][F]
+    // [][][ ]
+    val test_board = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Hidden), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(Player(PlayerID(1), new_player_board, PlayerColor(PrinterEffects.Blue)))), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    assert(all_mines_flagged(test_board, solution_board) == false)
+  }
+
+  test("num_flagged_mines_per_player_2_0") {
+    // [   ][     ][F.0]
+    // [   ][     ][F.0]
+    // [   ][1|F.1][   ]
+    val test_board_0 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.RNF(SolutionTile.Hint(1), player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+
+    // [   ][ 2 ][F.0]
+    // [   ][   ][F.0]
+    // [   ][F.1][   ]
+    val test_board_1 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Revealed(SolutionTile.Hint(2))), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Flagged(player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+    val test_player_0_1 = Player(PlayerID(0), test_board_0, PlayerColor(PrinterEffects.Red))
+    val test_player_1_1 = Player(PlayerID(1), test_board_1, PlayerColor(PrinterEffects.Blue))
+    
+    assert(num_flagged_mines_per_player(PlayerPool(List(test_player_0_1, test_player_1_1), 1), solution_board) == List(2, 0))
+  }
+
+  test("num_flagged_mines_per_player_1_1") {
+    // [   ][     ][F.0]
+    // [   ][     ][F.1]
+    // [   ][1|F.1][   ]
+    val test_board_0 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.RNF(SolutionTile.Hint(1), player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+
+    // [   ][ 2 ][F.0]
+    // [   ][   ][F.1]
+    // [   ][F.1][   ]
+    val test_board_1 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Revealed(SolutionTile.Hint(2))), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Flagged(player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+    val test_player_0_1 = Player(PlayerID(0), test_board_0, PlayerColor(PrinterEffects.Red))
+    val test_player_1_1 = Player(PlayerID(1), test_board_1, PlayerColor(PrinterEffects.Blue))
+    
+    assert(num_flagged_mines_per_player(PlayerPool(List(test_player_0_1, test_player_1_1), 1), solution_board) == List(1, 1))
+  }
+
+  test("has_won_1-all-mines-flagged-by-player0") {
+    // [][][F.0]
+    // [][][F.0]
+    // [][][   ]
+    val test_board_0 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+
+    // [][2][F.0]
+    // [][ ][F.0]
+    // [][ ][   ]
+    val test_board_1 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Flagged(player_1)), (Coordinate(1,2) -> PlayerTile.Revealed(SolutionTile.Hint(2))), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+    val test_player_0_1 = Player(PlayerID(0), test_board_0, PlayerColor(PrinterEffects.Red))
+    val test_player_1_1 = Player(PlayerID(1), test_board_1, PlayerColor(PrinterEffects.Blue))
+    
+    // assert(all_mines_flagged(test_player_0_1.board, solution_board) == true)
+    assert(has_won_1(PlayerPool(List(test_player_0_1, test_player_1_1), 1), 2, solution_board) == GameStatus.Win(Set(test_player_0_1)))
+  }
+
+  test("has_won_1-tie") {
+    // [][][F.0]
+    // [][][F.1]
+    // [][][   ]
+    val test_board_0 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+
+    // [][2][F.0]
+    // [][ ][F.1]
+    // [][ ][   ]
+    val test_board_1 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Flagged(player_1)), (Coordinate(1,2) -> PlayerTile.Revealed(SolutionTile.Hint(2))), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Hidden), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+    val test_player_0_1 = Player(PlayerID(0), test_board_0, PlayerColor(PrinterEffects.Red))
+    val test_player_1_1 = Player(PlayerID(1), test_board_1, PlayerColor(PrinterEffects.Blue))
+    
+    // assert(all_mines_flagged(test_player_0_1.board, solution_board) == true)
+    assert(has_won_1(PlayerPool(List(test_player_0_1, test_player_1_1), 1), 2, solution_board) == GameStatus.Win(Set(test_player_0_1, test_player_1_1)))
+  }
+
+  test("num_flagged_non_mines_per_player") {
+    // [   ][     ][F.0]
+    // [   ][     ][F.1]
+    // [   ][1|F.1][   ]
+    val test_board_0 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Hidden), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.RNF(SolutionTile.Hint(1), player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+
+    // [   ][ 2 ][F.0]
+    // [   ][   ][F.1]
+    // [   ][F.1][   ]
+    val test_board_1 = Board[PlayerTile](3, 3, Map(
+      (Coordinate(1,1) -> PlayerTile.Hidden), (Coordinate(1,2) -> PlayerTile.Revealed(SolutionTile.Hint(2))), (Coordinate(1,3) -> PlayerTile.Flagged(player_0)), 
+      (Coordinate(2,1) -> PlayerTile.Hidden), (Coordinate(2,2) -> PlayerTile.Hidden), (Coordinate(2,3) -> PlayerTile.Flagged(player_1)), 
+      (Coordinate(3,1) -> PlayerTile.Hidden), (Coordinate(3,2) -> PlayerTile.Flagged(player_1)), (Coordinate(3,3) -> PlayerTile.Hidden)
+    ))
+    
+    val test_player_0_1 = Player(PlayerID(0), test_board_0, PlayerColor(PrinterEffects.Red))
+    val test_player_1_1 = Player(PlayerID(1), test_board_1, PlayerColor(PrinterEffects.Blue))
+    
+    assert(num_flagged_non_mines_per_player(PlayerPool(List(test_player_0_1, test_player_1_1), 1), solution_board) == List(0, 1))
+  }
+*/
+}
